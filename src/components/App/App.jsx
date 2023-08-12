278e1a01dad95652d221dba411e4bb407a75505c
ivan-niceman <sn.iv@bk.ru>
2023-08-12 13:09:08 +0700

    изменил адрес api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import { api } from "../../utils/MainApi";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import ProtectedRoute from "../../utils/ProtectedRoute/ProtectedRoute";
 import Header from "../Header/Header";
--- src/utils/MainApi.jsx
+++ src/utils/MainApi.jsx
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://nice-man.diploma.nomoreparties.sbs";
+// export const BASE_URL = "https://nice-man.diploma.nomoreparties.sbs";
+const BASE_URL = 'https://api.nice-man.diploma.nomoreparties.sbs';
 
 const makeRequest = (url, method, body, token) => {
   const options = {
